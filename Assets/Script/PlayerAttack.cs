1561b7c04d30b1e98191e4844a84dec845f45ec8
Windywyll <appleteensee@yahoo.fr>
2016-01-13 12:17:32 +0100

    Commit CHaracter and its animation + basic movement (without attack and hanging)

---
+++ Assets/Script/PlayerAttack.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+    private bool attacking = false;
+    private bool canMove = true;
+    private PlayerControler pc;
+    private PlayerGrab pg;
+
+	// Use this for initialization
+	void Start () {
+        pc = this.gameObject.GetComponent<PlayerControler>();
+        pg = this.gameObject.GetComponent<PlayerGrab>();
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+
+    }
+
+    public void cantMove()
+    {
+        canMove = false;
+    }
+
+    public void freeMove()
+    {
+        canMove = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        if(canMove)
+        {
+            if(Input.GetButtonDown("Attack"))
+            {
+                //pc.cantMove();
+                //pg.cantMove();
+
+                attacking = true;
+            }
+
+            if(!attacking)
+            {
+                pc.freeMove();
+                pg.freeMove();
+            }
+        }
+
+	}
+}
---
+++ Assets/Script/PlayerGrab.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGrab : MonoBehaviour {
+
+    public int maxStamina = 2000;
+    private int stamina;
+    private bool hanging = false;
+    private int canHangItself = 0;
+    private bool canMove = true;
+    private PlayerControler pc;
+    private PlayerAttack pa;
+
+	// Use this for initialization
+	void Start () {
+        stamina = maxStamina;
+
+        pc = this.gameObject.GetComponent<PlayerControler>();
+        pa = this.gameObject.GetComponent<PlayerAttack>();
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "HangingPoint")
+        {
+            canHangItself++;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "HangingPoint")
+        {
+            canHangItself--;
+        }
+    }
+
+    public void cantMove()
+    {
+        canMove = false;
+    }
+
+    public void freeMove()
+    {
+        canMove = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        if(canHangItself > 0 && canMove)
+        {
+            if(Input.GetButton("Hang"))
+            {
+                pc.cantMove();
+                pa.cantMove();
+
+                hanging = true;
+                stamina--;
+            }
+
+            if(Input.GetButtonUp("Hang") || stamina == 0 )
+            {
+                hanging = false;
+
+                pc.freeMove();
+                pa.freeMove();
+            }
+        }
+        
+        if(!hanging && stamina < maxStamina)
+        {
+            stamina++;
+        }
+
+	}
+}
