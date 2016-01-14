356547f4e24cd3117a76772a729badc7b4aa352d
unknown <ce.brun@J001-0128-04.etudiant.rubika-edu.com.local>
2016-01-14 17:41:13 +0100

    Menus

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject credits;
+    public GameObject mainMenu;
+    
+    public void hideAll()
+    {
+        credits.SetActive(false);
+        mainMenu.SetActive(false);
+    }
+
+    public void onClickBack()
+    {
+        hideAll();
+        mainMenu.SetActive(false);
+    }
+    public void onClickPlay()
+    {
+        hideAll();
+    }
+    public void onClickCredits()
+    {
+        hideAll();
+        credits.SetActive(false);
+    }
+    public void onClickExit()
+    {
+        Application.Quit();
+    }
+}
